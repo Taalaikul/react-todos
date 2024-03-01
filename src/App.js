b6767568c849a0dd0b5b197faa025969b7cdec94
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-03-01 16:58:34 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import './App.css';
+import TodoTable from './components/TodoTable';
 
 function App() {
+
+  const todos = [
+    {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
+    {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
+    {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'}
+  
+  ]
+
   return (
     <div className = 'mt-5 container'>
       <div className = "card">
         <div className = "card-header">
           Your Todo's
         </div>
-        <div className = "card-body">
-          <table className="tabel table-hover">
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Description</th>
-                <th scope='col'>Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>Feed dog</td>
-                <td>Eric</td>
-              </tr>
-              <tr>
-              <th scope='row'>2</th>
-                <td>Get haircut</td>
-                <td>Eric</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className = 'card-body'>
+          <TodoTable todos={todos}/>
         </div>
       </div>
      
