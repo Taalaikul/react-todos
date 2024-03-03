c10b62c5c8b2857018937c7e111602192f1e0f01
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-03-03 10:48:29 -0500

    form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
+
+  const[ showAddTodoForm, setShowAddTodoForm] = useState(false);
 
   const [todos, setTodos] = useState([
     {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
@@ -12,16 +15,26 @@ function App() {
   ]
   )
 
-  const addTodo = () =>
-{
+  const addTodo = (description, assigned) =>{
+    let rowNumber = 0;
   if(todos.length > 0){
+    rowNumber = todos[todos.length - 1].rowNumber + 1;
+  } else{
+    rowNumber = 1;
+  }
     const newTodo = { 
-      rowNumber: todos.length + 1, 
-      rowDescription: 'New todo',
-      rowAssigned: 'User Three'
+      rowNumber: rowNumber, 
+      rowDescription: description,
+      rowAssigned: assigned
       };
       setTodos(todos => [...todos, newTodo])
-  }
+}
+
+const deleteTodo = (deleteTodoRowNumber) => {
+  let filtered = todos.filter(function (value){
+    return value.rowNumber !== deleteTodoRowNumber;
+  });
+  setTodos(filtered);
 }
 
 
@@ -32,10 +45,12 @@ function App() {
           Your Todo's
         </div>
         <div className = 'card-body'>
-          <TodoTable todos={todos}/>
-          <button className='btn btn-primary' onClick = {addTodo}>
-            Add new todo
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary'>
+            {showAddTodoForm ? 'Close New Todo': 'New Todo'}
             </button>
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo}/>}
+            
         </div>
       </div>
      
